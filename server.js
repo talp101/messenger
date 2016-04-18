83d2670a429e09b6209d0127d0636e181087efec
Tal Peretz <13@1500.co.il>
2016-04-19 00:26:47 +0300

    initial project setup

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use('/static', express.static('static'));
+
+app.get('/' , (request, response) => {
+    response.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(3000, (err)=>{
+    if(err)
+        console.log(err)
+    console.log('server started at port 3000');
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var ROOT_PATH = path.resolve(__dirname, 'static');
+module.exports = {
+    entry: path.resolve(ROOT_PATH, 'src/index'),
+    output: {
+        path: path.join(ROOT_PATH, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loaders: ['style', 'css'],
+                include: path.resolve(ROOT_PATH, 'src/css')
+            },
+            {
+                test: /\.(js|jsx|es6)$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'stage-0', 'react'],
+                    plugins: ["transform-decorators"]
+                }
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=100000",
+                include: path.resolve(ROOT_PATH, 'src/img')
+            }
+        ]
+    }
+};
