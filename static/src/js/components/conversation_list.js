137f7edf3a4b27ffae3abd8d6f6cfeeaf8bc5a6f
Tal Peretz <13@1500.co.il>
2016-04-25 23:04:11 +0300

    update design

--- static/src/js/components/conversation_list.js
+++ static/src/js/components/conversation_list.js
@@ -10,9 +10,8 @@ class ConversationsList extends Component {
         const {conversations, user, dispatch} = this.props;
         let conversationsList = conversations.data.map((conversation) => {
             return (
-
                 <li key={conversation._id} id={conversation._id} aria-layout="column" aria-layout-align="end end" className="collection-item avatar" onClick={this.openConversation.bind(this)}>
-                    <i className="material-icons circle indigo darken-3">face</i>
+                    <i className="material-icons circle messenger-background">face</i>
                     <div className="conversation-main" aria-layout="column" aria-layout-align="start end">
                     <span className="title">{conversation.between.filter(user1 => user1._id != user._id)[0].lastName}</span>
                     <p className="brown-text text-lighten-3" aria-layout="row" aria-layout-align="end end">
@@ -28,8 +27,8 @@ class ConversationsList extends Component {
         return (
             <div className="grey lighten-5" aria-layout="column" aria-flex="25" aria-layout-align="start end" aria-layout-fill>
                 <ul className="right side-nav fixed contacts_menu" aria-layout-fill>
-                    <li className="indigo lighten-1 white-text w-full h-xs" aria-layout="row" aria-layout-align="center center">
-                        <span className="brown-text text-lighten-4 text-1-5x m-l">Messenger</span>
+                    <li className="messenger-background white-text w-full h-xs" aria-layout="row" aria-layout-align="center center">
+                        <span className="white-text text-1-5x m-l">Messenger</span>
                     </li>
                     <li aria-layout="column" className="wrapper w-full" aria-layout-align="center center">
                         <div aria-layout="row" aria-layout-align="center center">
--- static/src/js/components/index_page.js
+++ static/src/js/components/index_page.js
@@ -4,8 +4,9 @@ import React, {Component, PropTypes} from 'react';
 class IndexPage extends Component{
     render(){
         return (
-            <div>
-                <h1>welcome</h1>
+            <div className="index-page-wrapper" aria-layout="column" aria-fill-layout aria-flex="75">
+                <h1 className="index-title">פשוט לדבר</h1>
+                <img height="25%" width="25%" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Facebook_Messenger_logo.svg/2000px-Facebook_Messenger_logo.svg.png" className="responsive-img messenger-logo"/>
             </div>
 
         )
